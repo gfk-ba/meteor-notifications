808ac606524a8f8f809f16764438b89d404a7788
Marco de Jongh <marco.dejongh@gfk.com>
2014-11-17 11:23:33 +0100

    Removed rendered helper on notification template because it was no longer necessary

--- notification.js
+++ notification.js
@@ -1,10 +1,5 @@
 'use strict';
 
-Template.notification.rendered = function () {
-    //TODO: Remove this when meteor issue #2369 gets fixed
-    $(this.firstNode).data('_id', this.data._id);
-};
-
 Template.notification.helpers({
     notificationColor: function(notificationType) {
         return Notifications.getNotificationClass(notificationType);
